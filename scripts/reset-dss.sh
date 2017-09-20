01a1ca7fbd0d7d551a3acc98e6a849c1560e9970
Michael Baumann <mbaumann@ucsc.edu>
2017-09-20 10:40:25 -0700

    Changed gsutil rm to use DSS_GS_BUCKET

--- scripts/reset-dss.sh
+++ scripts/reset-dss.sh
@@ -5,5 +5,5 @@ DSS_HOME="$(cd -P "$(dirname "$SOURCE")/.." && pwd)"
 
 ${DSS_HOME}/scripts/s3rmll.py s3://${DSS_S3_BUCKET}/bundles
 ${DSS_HOME}/scripts/s3rmll.py s3://${DSS_S3_BUCKET}/files
-gsutil -m rm -r gs://${DSS_S3_BUCKET}/bundles/*
-gsutil -m rm -r gs://${DSS_S3_BUCKET}/files/*
+gsutil -m rm -r gs://${DSS_GS_BUCKET}/bundles/*
+gsutil -m rm -r gs://${DSS_GS_BUCKET}/files/*
